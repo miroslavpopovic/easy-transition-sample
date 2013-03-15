674555b2d72a2f3416f337ff14ed9d9dda83c9ee
Miroslav Popovic <miroslav.popovic@gmail.com>
2013-03-15 14:40:31 +0100

    Installed Bootstrap via NuGet, added few initial views

---
+++ src/KnockoutSamples/Extensions/HtmlHelperExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Web.Mvc;
+
+namespace KnockoutSamples.Extensions
+{
+    public static class HtmlHelperExtensions
+    {
+         public static string ActiveIfMatch(this HtmlHelper helper, string action, string controller)
+         {
+             var routeValues = helper.ViewContext.RouteData.Values;
+             var currentAction = routeValues["action"].ToString().ToLower();
+             var currentController = routeValues["controller"].ToString().ToLower();
+
+             return (action.ToLower() == currentAction && controller.ToLower() == currentController)
+                        ? "active"
+                        : String.Empty;
+         }
+    }
+}
