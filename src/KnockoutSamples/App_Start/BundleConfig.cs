f1c8d387561b8029faa4000a404cb7bcb1b68b23
Miroslav Popovic <miroslav.popovic@gmail.com>
2013-03-18 12:39:02 +0100

    Added initial ShoppingCart implementation

--- src/KnockoutSamples/App_Start/BundleConfig.cs
+++ src/KnockoutSamples/App_Start/BundleConfig.cs
@@ -18,6 +18,12 @@ namespace KnockoutSamples
                         "~/Scripts/jquery.unobtrusive*",
                         "~/Scripts/jquery.validate*"));
 
+            bundles.Add(new ScriptBundle("~/bundles/knockout").Include(
+                "~/Scripts/globalize.js",
+                "~/Scripts/knockout-{version}.js",
+                "~/Scripts/knockout.extensions.js",
+                "~/Scripts/sampleProductCategories.js"));
+
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
