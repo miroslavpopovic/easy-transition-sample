523fc7a8be2b53520df779e380f8482310838ee8
Miroslav Popovic <miroslav.popovic@gmail.com>
2013-03-18 15:15:02 +0100

    select2 binding simplified for the demo

--- src/KnockoutSamples/Scripts/knockout.extensions.js
+++ src/KnockoutSamples/Scripts/knockout.extensions.js
@@ -87,22 +87,15 @@
 
 ko.bindingHandlers.select2 = {
     init: function (element, valueAccessor, allBindingsAccessor) {
-        var obj = valueAccessor(),
-            allBindings = allBindingsAccessor(),
-            lookupKey = allBindings.lookupKey;
+        var obj = valueAccessor();
+
         $(element).select2(obj);
-        if (lookupKey) {
-            var value = ko.utils.unwrapObservable(allBindings.value);
-            $(element).select2('data', ko.utils.arrayFirst(obj.data.results, function (item) {
-                return item[lookupKey] === value;
-            }));
-        }
 
         ko.utils.domNodeDisposal.addDisposeCallback(element, function () {
             $(element).select2('destroy');
         });
     },
-    update: function (element) {
+    update: function (element, valueAccessor, allBindingsAccessor) {
         $(element).trigger('change');
     }
 };
